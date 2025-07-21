9fa5ef0dfabe69666a1f99c3a97e7d141dadac3b
Admin <Admin@DSE-UNAA3SI03840504FBC0700>
2025-07-21 22:55:30 +0530

    Identifiers

---
+++ C2TC_GAYATHRI/src/day1_Identifier/Identifier.java
@@ -0,0 +1,12 @@
+package day1_Identifier;
+
+	public class Identifier {
+		public static void main(String[] args){
+		int $number = 10;
+		System.out.println("value of the number variable is : "+ $number);
+
+		String studentName="Udhaya"; 
+		System.out.println("value is : "+ studentName);
+	}
+	}
+	
