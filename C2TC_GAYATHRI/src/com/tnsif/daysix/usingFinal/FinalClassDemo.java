09b7b4ca41c837a56e6fe6741b016462685955ff
Admin <Admin@DSE-UNAA3SI03840504FBC0700>
2025-07-28 09:47:58 +0530

    day6

--- C2TC_GAYATHRI/src/com/tnsif/daysix/usingFinal/FinalClassDemo.java
+++ C2TC_GAYATHRI/src/com/tnsif/daysix/usingFinal/FinalClassDemo.java
@@ -1,5 +1,17 @@
 package com.tnsif.daysix.usingFinal;
 
-public class FinalClassDemo {
+final class FinalClass {
+	void show() {
+		System.out.println("Final class cannot be inherited");
+	}
+}
 
+// ❌ This will cause error because FinalClass is final
+// class FinalChildClass extends FinalClass { }
+
+public class FinalClassDemo {
+	public static void main(String[] args) {
+		FinalClass f1 = new FinalClass();
+		f1.show();
+	}
 }
